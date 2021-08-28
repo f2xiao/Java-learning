df9177984f0ff50efb8d1fd3cbe0a2f959aab6a3
f2xiao <fxiao.mecheng@gmail.com>
2021-08-28 17:35:03 -0400

    add bubble sort and selection sort

--- Basics/src/Syntax/ConstantDemo.java
+++ Basics/src/Syntax/ConstantDemo.java
@@ -1,2 +1,22 @@
-package Syntax;public class ConstantDemo {
+package Syntax;
+
+import org.w3c.dom.ls.LSOutput;
+
+/*
+Constant: whose value are not changed
+String: "Hello World";
+Integers: 22;
+Decimals: 2.3;
+Char: 'A', 'a';
+Boolean: true, false;
+Null: null;
+*/
+public class ConstantDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
 }
--- Basics/src/Syntax/VariableDemo.java
+++ Basics/src/Syntax/VariableDemo.java
@@ -1,2 +1,46 @@
-package Syntax;public class VariableDemo {
+package Syntax;
+/*
+dataType variableName = value;
+Basic data types: byte, short, char, int, long, float, double, boolean
+
+ */
+public class VariableDemo {
+    public static void main(String[] args) {
+        byte a = 10;
+        System.out.println(a);
+        System.out.println("-------------------");
+
+        short b = 22;
+        System.out.println(b);
+        System.out.println("-------------------");
+
+        char c ='A';
+        System.out.println(c);
+        System.out.println("-------------------");
+
+        int d = 220;
+        System.out.println(d);
+        System.out.println("-------------------");
+
+        long e = 100000010L;
+        System.out.println(e);
+        System.out.println("-------------------");
+
+        float f = 3.14f;
+        System.out.println(f);
+        System.out.println("-------------------");
+
+        double g = 1.333333333;
+        System.out.println(g);
+        System.out.println("-------------------");
+
+        boolean h = false ;
+        System.out.println(h);
+        System.out.println("-------------------");
+
+        h = true;
+        System.out.println(h);
+        System.out.println("-------------------");
+    }
+
 }

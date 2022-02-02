5178ce45ad6b4e0976a2cf13fcf5bf039a89de7d
f2xiao <fxiao.mecheng@gmail.com>
2022-02-02 15:36:41 -0500

    Edit HelloNumbers

--- Basics/src/Syntax/HelloNumbers.java
+++ Basics/src/Syntax/HelloNumbers.java
@@ -1,18 +1,31 @@
 package Syntax;
 
 public class HelloNumbers {
-    public static void main(String[] args) {
+    public static int sum(int num) {
         int x = 0, sum = 0;
-        while (x < 10) {
+        while (x < num) {
             System.out.print(sum + " ");
             x++;
             sum += x;
         }
-
-        System.out.println("\n--------");
-        String h = 5 + "horse";
-        System.out.println(h);
-
+        return sum;
     }
 
+    public static boolean isPrime(int n) {
+        int divisor = 2;
+        while(divisor < n){
+            if (n % divisor == 0){
+                return false;
+            }
+            divisor++;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+//         sum(10);
+         boolean isPrime = isPrime(10);
+        System.out.println(isPrime);
+    }
 }

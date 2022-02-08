aab97a4aa11829d2b82415478b6f0cbfc24fc479
f2xiao <fxiao.mecheng@gmail.com>
2022-02-08 13:27:25 -0500

    Add IntList iterativeSize method using iteration

--- Basics/src/List/IntList.java
+++ Basics/src/List/IntList.java
@@ -17,4 +17,15 @@ public class IntList {
         return 1+ this.rest.size();
     }
 
+    /** Return size of this IntList, using iteration, no recursion*/
+    public int iterativeSize(){
+        int totalSize =0;
+        IntList p = this;
+        while(p != null){
+            totalSize ++;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
 }

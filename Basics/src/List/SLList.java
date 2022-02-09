484b589b9364efcc722abdab29636b922a3a38b8
f2xiao <fxiao.mecheng@gmail.com>
2022-02-09 16:04:41 -0500

    Add SLList private class IntNode

---
+++ Basics/src/List/SLList.java
@@ -0,0 +1,13 @@
+package List;
+
+public class SLList {
+    private class IntNode{
+        public int item;
+        public IntNode next;
+        public IntNode(int i, IntNode n){
+            item = i;
+            next = n;
+        }
+    }
+
+}

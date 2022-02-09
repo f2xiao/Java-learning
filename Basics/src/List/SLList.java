6674fe6ba68346062763a9344d6c4279c98bb1c2
f2xiao <fxiao.mecheng@gmail.com>
2022-02-09 16:10:18 -0500

    Add SLList addFirst and getFirst method

--- Basics/src/List/SLList.java
+++ Basics/src/List/SLList.java
@@ -15,4 +15,18 @@ public class SLList {
         first = new IntNode(x, null);
     }
 
+    public void addFirst(int x){
+        first = new IntNode(x, first);
+    }
+
+    public int getFirst(){
+        return first.item;
+    }
+
+    public static void main(String[] args) {
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(5);
+    }
+
 }

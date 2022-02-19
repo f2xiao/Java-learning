379c1817abf35c5eb863685654b1eef7152a9511
f2xiao <fxiao.mecheng@gmail.com>
2022-02-18 19:53:08 -0500

    Update Sort and TestSort

--- Basics/src/Sort/Sort.java
+++ Basics/src/Sort/Sort.java
@@ -2,6 +2,29 @@ package Sort;
 
 public class Sort {
     /** Sorts strings destructively. */
+    /*
+    * Find the smallest item.
+    * Move it to the front.
+    * Selection sort the remaining N-1 items (without touching the front item)*/
+    public static int findSmallest(String[] x, int i){
+        int smallestIndex = i;
+        for (int j = i; j < x.length; j++) {
+            if(x[j].compareTo(x[smallestIndex])<0){
+                smallestIndex = j;
+            }
+        }
+        return smallestIndex;
+
+    }
+    public static void swap(String[] x, int i, int j){
+        String temp = x[i];
+        x[i] = x[j];
+        x[j] = temp;
+    }
     public static void sort(String[] x) {
+        for (int i = 0; i < x.length; i++) {
+            int smallestIndex = findSmallest(x, i);
+            swap(x, i, smallestIndex);
+        }
     }
 }
--- Basics/src/Testing/TestSort.java
+++ Basics/src/Testing/TestSort.java
@@ -1,12 +1,13 @@
 package Testing;
-import Sort.SelectionSort;
+import Sort.Sort;
+
 
 public class TestSort {
     public static void testSort() {
-        int[] input = new int[]{5,4,3,2,1};
-        int[] expected = new int[]{1,2,3,4,5} ;
+        String[] input1 = {"i", "have","a","cat", "called", "pixie"};
+        String[] expected1 = {"a", "called","cat","have", "i", "pixie"};
 
-        SelectionSort.sort(input);
+        Sort.sort(input1);
         // without junit library
         /*for (int i = 0; i < input.length; i++) {
             if(!input[i].equals(expected[1])){
@@ -16,7 +17,14 @@ public class TestSort {
         }*/
 
         // with junit library
-        org.junit.Assert.assertArrayEquals(expected, input);
+        org.junit.Assert.assertArrayEquals(expected1, input1);
+
+        String[] input2 = {"my", "cat","likes","tuna"};
+        String[] expected2 = {"cat", "likes","my","tuna"};
+
+        Sort.sort(input2);
+        org.junit.Assert.assertArrayEquals(expected2, input2);
+
     }
 
     public static void main(String[] args) {

2b98b27798485aff1a1634613cda40479e60371f
f2xiao <fxiao.mecheng@gmail.com>
2022-02-24 13:27:47 -0500

    Update TestSort

--- Basics/src/Testing/TestSort.java
+++ Basics/src/Testing/TestSort.java
@@ -1,9 +1,11 @@
 package Testing;
 import Sort.Sort;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestSort {
-    public static void testSort() {
+    @Test
+    public void testSort() {
         String[] input1 = {"i", "have","a","cat", "called", "pixie"};
         String[] expected1 = {"a", "called","cat","have", "i", "pixie"};
 
@@ -17,17 +19,29 @@ public class TestSort {
         }*/
 
         // with junit library
-        org.junit.Assert.assertArrayEquals(expected1, input1);
+        assertArrayEquals(expected1, input1);
 
         String[] input2 = {"my", "cat","likes","tuna"};
         String[] expected2 = {"cat", "likes","my","tuna"};
 
         Sort.sort(input2);
-        org.junit.Assert.assertArrayEquals(expected2, input2);
+        assertArrayEquals(expected2, input2);
 
     }
-
-    public static void main(String[] args) {
-        testSort();
+    @Test
+    public void testFindSmallest(){
+        String[] input = {"i", "have","a","cat", "called", "pixie"};
+        int expected = 4;
+        int actual=Sort.findSmallest(input, 3);
+        assertEquals(expected, actual);
     }
+    @Test
+    public void testSwap(){
+        String[] input = {"i", "have","a","cat", "called", "pixie"};
+        String[] expected = {"a", "have","i","cat", "called", "pixie"};
+
+        Sort.swap(input, 0, 2);
+        assertArrayEquals(expected, input);
+    }
+
 }

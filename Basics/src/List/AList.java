786b21e3d30cbc01093284ef0b82faba94490feb
f2xiao <fxiao.mecheng@gmail.com>
2022-03-08 18:06:59 -0500

    Edit AList to make it generic type

---
+++ Basics/src/List/AList.java
@@ -0,0 +1,58 @@
+package List;
+
+/** Array based list.
+ *  @author Josh Hug
+ */
+
+/* Invariants
+* The position of the next item to be inserted(using addLAst) is always size.
+* The number of the items in the Alist is always size
+* The position of the last item in the list is always size-1
+* */
+public class AList<Item> {
+    private Item[] items;
+    private int size;
+    /** Creates an empty list. */
+    public AList() {
+       items = (Item []) new Object[100];
+       size =0;
+    }
+
+    private void resize(int capacity){
+        Item[] a= (Item []) new Object[size+capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+
+    /** Inserts X into the back of the list. */
+    public void addLast(Item x) {
+        if(size==items.length) {
+           resize(size*2);
+        }
+        items[size] = x;
+        size++;
+    }
+
+    /** Returns the item from the back of the list. */
+    public Item getLast() {
+        return items[size-1];
+    }
+    /** Gets the ith item in the list (0 is the front). */
+    public Item get(int i) {
+        return items[i];
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Deletes item from back of the list and
+     * returns deleted item. */
+    public Item removeLast() {
+        Item last = items[size-1];
+        items[size-1] = null;
+        size--;
+        return last;
+    }
+}

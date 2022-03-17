c96f7256c55c5d5aec5830bb179cd03c78a70a7c
f2xiao <fxiao.mecheng@gmail.com>
2022-03-17 12:48:01 -0400

    Edit Dog main

--- Basics/src/Casting/Dog.java
+++ Basics/src/Casting/Dog.java
@@ -7,11 +7,17 @@ public class Dog {
         name = n;
         weight = w;
     }
-    public Dog maxDog(Dog d1, Dog d2){
+    public static Dog maxDog(Dog d1, Dog d2){
         Dog max = d1;
         if(d2.weight > max.weight){
             max = d2;
         }
         return max;
-    };
+    }
+
+    public static void main(String[] args) {
+        Poddle frank = new Poddle("Frank", 5);
+        Malamute frankSr = new Malamute("Frank Sr.", 100);
+        Dog largerPoddle =  maxDog(frank, frankSr);
+    }
 }

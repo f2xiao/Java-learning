9299892828e6bc3cef42db8b038370f0058cf463
f2xiao <fxiao.mecheng@gmail.com>
2022-03-19 00:22:44 -0400

    Edit Dog class

--- Basics/src/Inheritance/Dog.java
+++ Basics/src/Inheritance/Dog.java
@@ -1,5 +1,7 @@
 package Inheritance;
 
+import java.util.Comparator;
+
 import static Inheritance.Maximizer.max;
 
 public class Dog implements Comparable<Dog> {
@@ -39,10 +41,32 @@ public class Dog implements Comparable<Dog> {
         return this.size - uddaDog.size;
     }
 
+    private static class NameComparator implements Comparator<Dog>{
+        @Override
+        public int compare(Dog a, Dog b) {
+            return a.name.compareTo(b.name);
+        }
+    }
+
+    public static Comparator<Dog> getNameComparator(){
+        return new NameComparator();
+    }
+
+
+
 
     public static void main(String[] args) {
-        Dog[] dogs = {new Dog("Alfie", 3),new Dog("Bat", 9),new Dog("Cat", 15) };
-        Dog maxDog = (Dog) max(dogs);
-        maxDog.bark();
+        Dog d1 =new Dog("Alfie", 3);
+        Dog d2 =new Dog("Cat", 9);
+        Dog d3 =new Dog("Bat", 15) ;
+        Dog[] dogs = {d1,d2,d3};
+        Comparator<Dog> nc = Dog.getNameComparator();
+//        Dog maxDog = (Dog) max(dogs);
+//        maxDog.bark();
+        if(nc.compare(d1,d3) >0){
+            d1.bark();
+        }else{
+            d3.bark();
+        }
     }
 }

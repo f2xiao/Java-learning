700c2455bd2f50d169a25e097c4f937449fd48d3
f2xiao <fxiao.mecheng@gmail.com>
2022-04-22 00:37:53 -0400

    Edit OHQueue

--- Basics/src/Queue/OHQueue.java
+++ Basics/src/Queue/OHQueue.java
@@ -74,15 +74,15 @@ public class OHQueue implements Iterable<OHQueue.OHRequest> {
     /** returns an iterator with cur as the first item in the queue */
     @Override
     public Iterator<OHRequest> iterator() {
-        return new OHIterator(cur);
+        return new TYIterator(cur);
     }
 
     public static void main(String[] args) {
         OHRequest s5 = new OHRequest("I deleted all of my files", "Allyson", null);
         OHRequest s4 = new OHRequest("conceptual: what is Java", "Omar", s5);
         OHRequest s3 = new OHRequest("git: I never did lab 1", "Connor", s4);
-        OHRequest s2 = new OHRequest("help", "Hug", s3);
-        OHRequest s1 = new OHRequest("no I haven't tried stepping through", "Itai", s2);
+        OHRequest s2 = new OHRequest("thank u", "Hug", s3);
+        OHRequest s1 = new OHRequest("thank u", "Itai", s2);
 
         /* makes a new OHQueue object*/
         OHQueue q = new OHQueue(s1);
